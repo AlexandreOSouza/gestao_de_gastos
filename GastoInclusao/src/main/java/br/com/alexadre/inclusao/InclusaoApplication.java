fb2fc84f8e49d151d5fbf70c84bab7bb3d497735
alexandreSnow <alexandre.souza64@fatec.sp.gov.br>
2020-03-16 05:12:52 -0300

    Crianção do serviço para a inclusão dos gastos

---
+++ GastoInclusao/src/main/java/br/com/alexadre/inclusao/InclusaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.alexadre.inclusao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InclusaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InclusaoApplication.class, args);
+	}
+
+}
