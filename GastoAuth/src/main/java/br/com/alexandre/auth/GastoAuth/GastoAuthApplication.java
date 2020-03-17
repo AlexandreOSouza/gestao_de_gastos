e2301cbb59f384af1f2bd36c45c10b5ebf64c407
alexandreSnow <alexandre.souza64@fatec.sp.gov.br>
2020-03-17 11:09:49 -0300

    Criação do serviço de autenticação

---
+++ GastoAuth/src/main/java/br/com/alexandre/auth/GastoAuth/GastoAuthApplication.java
@@ -0,0 +1,13 @@
+package br.com.alexandre.auth.GastoAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastoAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GastoAuthApplication.class, args);
+	}
+
+}
