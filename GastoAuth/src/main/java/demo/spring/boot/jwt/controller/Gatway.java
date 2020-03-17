09c741f42136186d70a80f28cb40162fe1a45e8d
alexandreSnow <alexandre.souza64@fatec.sp.gov.br>
2020-03-17 13:43:11 -0300

    Criando fluxo de autenticação com token

---
+++ GastoAuth/src/main/java/demo/spring/boot/jwt/controller/Gatway.java
@@ -0,0 +1,18 @@
+package demo.spring.boot.jwt.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value= "/api", produces = { MediaType.APPLICATION_JSON_UTF8_VALUE })
+public class Gatway {
+
+	@RequestMapping(value = "listar_gastos", method = RequestMethod.GET)
+	public ResponseEntity<String> listarGasto() {
+		return new ResponseEntity<String>("Listando os gastos", HttpStatus.OK);
+	}
+}
